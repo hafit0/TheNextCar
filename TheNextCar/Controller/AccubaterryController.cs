9ec039d9927230d5dc3c5c6af163d181ecda0deb
hafit01 <hafit.abk@gmail.com>
2020-10-28 06:56:58 +0700

    Add project files.

---
+++ TheNextCar/Controller/AccubaterryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheNextCar.Model;
+
+namespace TheNextCar.Controller
+{
+    class AccubatteryController
+    {
+        private Accubattery accubattery;
+        private OnPowerChanged callBackOnPowerChanged;
+
+        public AccubatteryController(OnPowerChanged callBackOnPowerChanged)
+        {
+            this.accubattery = new Accubattery(12);
+            this.callBackOnPowerChanged = callBackOnPowerChanged;
+        }
+
+        public bool accubatteryIsOn()
+        {
+            return this.accubattery.isOn();
+        }
+
+        public void turnOn()
+        {
+            this.accubattery.turnOn();
+            this.callBackOnPowerChanged.onPowerChangedStatus("ON", "power is on");
+        }
+
+        public void turnOff()
+        {
+            this.accubattery.turnOff();
+            this.callBackOnPowerChanged.onPowerChangedStatus("OFF", "power is off");
+
+        }
+    }
+
+    interface OnPowerChanged
+    {
+        void onPowerChangedStatus(string value, string message);
+    }
+}
